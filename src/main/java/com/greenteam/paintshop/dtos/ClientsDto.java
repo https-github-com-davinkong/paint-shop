69e8ef8075f3e19d4659bf67b6e35417c5f608b9
jenniesf <jenniec2655@gmail.com>
2023-01-25 20:42:53 -0800

    update client entity and dto

---
+++ src/main/java/com/greenteam/paintshop/dtos/ClientsDto.java
@@ -0,0 +1,30 @@
+package com.greenteam.paintshop.dtos;
+
+import com.greenteam.paintshop.entities.Clients;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientsDto implements Serializable {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public ClientsDto(Clients clients){
+        if (clients.getId() != null){
+            this.id = clients.getId();
+        }
+        if (clients.getFirstName() != null){
+            this.firstName = clients.getFirstName();
+        }
+        if (clients.getLastName() != null){
+            this.lastName = clients.getLastName();
+        }
+    }
+}
--- src/main/java/com/greenteam/paintshop/entities/Clients.java
+++ src/main/java/com/greenteam/paintshop/entities/Clients.java
@@ -1,5 +1,6 @@
 package com.greenteam.paintshop.entities;
 
+import com.greenteam.paintshop.dtos.ClientsDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,11 +13,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Clients {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String firstName;
     @Column
     private String lastName;
 
+    public Clients(ClientsDto clientsDto) {
+        if(clientsDto.getFirstName() != null) {
+            this.firstName = clientsDto.getFirstName();
+        }
+        if(clientsDto.getLastName() != null) {
+            this.lastName = clientsDto.getLastName();
+        }
+    }
 }
