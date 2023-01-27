a639245f84705472bf35ba77b37228f9dc31d2b4
ChrisCluney <ccluney@gmail.com>
2023-01-26 22:09:56 -0600

    completed products entity and Dto

--- src/main/java/com/greenteam/paintshop/repositories/ProductsRepository.java
+++ src/main/java/com/greenteam/paintshop/repositories/ProductsRepository.java
@@ -1,2 +1,10 @@
-package com.greenteam.paintshop.repositories;public class ProductsRepository {
+package com.greenteam.paintshop.repositories;
+
+import com.greenteam.paintshop.entities.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductsRepository extends JpaRepository<Products, Long> {
 }
--- src/main/java/com/greenteam/paintshop/services/ProductsServiceImpl.java
+++ src/main/java/com/greenteam/paintshop/services/ProductsServiceImpl.java
@@ -1,2 +1,14 @@
-package com.greenteam.paintshop.services;public class ProductsServiceImpl {
+package com.greenteam.paintshop.services;
+
+//
+//import com.greenteam.paintshop.repositories.ProductsRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductsServiceImpl {
+//    @Autowired
+//    private JobsRepository jobsRepository;
+//   @Autowired
+//    private ProductsRepository productsRepository;
 }
