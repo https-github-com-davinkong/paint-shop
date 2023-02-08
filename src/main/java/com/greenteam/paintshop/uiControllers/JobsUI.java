a8d3ea40d938e49fd9aa3ac425f5f3ec2ccce12e
palomasoltys <pfs601@gmail.com>
2023-02-07 21:04:27 -0500

    Create Jobs HTML Form to add a Job. Create Jobs Controller

---
+++ src/main/java/com/greenteam/paintshop/uiControllers/JobsUI.java
@@ -0,0 +1,43 @@
+package com.greenteam.paintshop.uiControllers;
+
+import com.greenteam.paintshop.entities.Clients;
+import com.greenteam.paintshop.entities.Contractors;
+import com.greenteam.paintshop.entities.Jobs;
+import com.greenteam.paintshop.repositories.ClientsRepository;
+import com.greenteam.paintshop.repositories.ContractorsRepository;
+import com.greenteam.paintshop.repositories.JobsRepository;
+import com.greenteam.paintshop.repositories.ProductsRepository;
+import com.greenteam.paintshop.services.ClientsService;
+import com.greenteam.paintshop.services.ContractorsService;
+import com.greenteam.paintshop.services.JobsService;
+import com.greenteam.paintshop.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JobsUI {
+
+    @Autowired
+    private JobsRepository jobsRepository;
+
+    @Autowired
+    private ContractorsRepository contractorsRepository;
+
+    @Autowired
+    private ClientsRepository clientsRepository;
+
+    @Autowired
+    private ProductsRepository productsRepository;
+
+    @GetMapping("/jobs/new")
+    public String showForm(Model model) {
+//        model.addAttribute("client", new Clients());
+        model.addAttribute("clients", clientsRepository.findAll());
+        model.addAttribute("products", productsRepository.findAll());
+        model.addAttribute("contractors", contractorsRepository.findAll());
+        model.addAttribute("job", new Jobs());
+        return "jobs";
+    }
+}
