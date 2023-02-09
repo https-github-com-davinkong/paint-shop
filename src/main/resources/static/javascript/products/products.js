577f063bf1d86c69a091c653a49c2fd91b0226c1
ChrisCluney <ccluney@gmail.com>
2023-02-09 09:15:37 -0600

    Created products html page and products javaScript file

---
+++ src/main/resources/static/javascript/products/products.js
@@ -0,0 +1,51 @@
+//Cookie
+const cookieArr = document.cookie.split("=")
+const contractorsId = cookieArr[1];
+
+const headers = {
+    'Content-Type':'application/json'
+}
+
+const baseUrl = 'http://localhost:8080/api/v1/products'
+
+// GET DOM ELEMENTS
+const productForm = document.getElementById('product-form')
+const productPaintColor = document.getElementById('product-paintColor')
+const productDescription = document.getElementById('product-description')
+const productTools = document.getElementById('product-tools')
+
+// POST a product
+const handleSubmit = async (e) =>{
+    e.preventDefault()
+    let bodyObj = {
+        paintColor: productPaintColor.value,
+        description: productDescription.value,
+        tools: productTools.value,
+    }
+    const response = await fetch(`${baseUrl}/product`, {
+        method: "POST",
+        body: JSON.stringify(bodyObj),
+        headers: headers
+    })
+        .catch(err => console.error(err.message))
+//    const result = await response.json()
+    if (response.status == 200){
+        console.log("product added")
+    }
+    window.location.replace("http://localhost:8080/products")
+}
+
+productForm.addEventListener("submit", handleSubmit)
+
+
+// LOGOUT button
+function handleLogout(){
+    let c = document.cookie.split(";");
+
+    for(let i in c){
+        document.cookie = /^[^=]+/.exec(c[i])[0]+"=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    }
+
+}
+
+
